4f84f664d08affdd74d343d8ee48f08d68c601e6
svda <sander@myxt.nl>
2011-06-07 11:26:34 +0200

    Added settings for delubas.

---
+++ settings/siteaccess/delubas_admin/icon.ini.append.php
@@ -0,0 +1,10 @@
+<?php /* #?ini charset="utf8"?
+
+# ###### ICON SETTINGS: start ######################################### #
+[IconSettings]
+Repository=extension/driwebin_1_0-5/share/icons
+Theme=driwebin_admin_icons
+Size=normal
+# ###### ICON SETTINGS: end ########################################### #
+
+*/ ?>
---
+++ settings/siteaccess/delubas_admin/site.ini.append.php
@@ -0,0 +1,46 @@
+<?php /* #?ini charset="utf-8"?
+
+[DatabaseSettings]
+Server=127.0.0.1
+Database=delubas_www
+User=delubas
+Password=TemYztM5KtGvRcZm
+
+[SiteSettings]
+SiteName=Delubas beheeromgeving
+SiteURL=webadmin.delubas.nl
+DefaultPage=content/dashboard
+LoginPage=custom
+GMapsKey=ABQIAAAANHtIVWRL0sEBbnwJ7ZTqkxQ1Pukl7qFaZ3xZT-YCnLizLokq1xQ-LWXSiXp889ZDDFuKxbcMrCZcqA
+
+[SiteAccessSettings]
+RequireUserLogin=true
+RelatedSiteAccessList[]=delubas_nl
+RelatedSiteAccessList[]=boekpakket_nl
+RelatedSiteAccessList[]=delubas_admin
+ShowHiddenNodes=true
+
+[DesignSettings]
+SiteDesign=myxt_admin
+AdditionalSiteDesignList[]
+AdditionalSiteDesignList[]=ezflow_site_admin
+AdditionalSiteDesignList[]=admin2
+AdditionalSiteDesignList[]=admin
+AdditionalSiteDesignList[]=ezflow
+
+[RegionalSettings]
+Locale=dut-NL
+ContentObjectLocale=dut-NL
+ShowUntranslatedObjects=enabled
+SiteLanguageList[]
+SiteLanguageList[]=dut-NL
+SiteLanguageList[]=eng-GB
+TextTranslation=enabled
+
+[FileSettings]
+VarDir=var/delubas
+
+[ContentSettings]
+CachedViewPreferences[full]=admin_navigation_content=1;admin_children_viewmode=list;admin_list_limit=1
+
+ */ ?>
