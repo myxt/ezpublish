bd25beae365c5dc7e02969c278209f0c2d813d21
svda <sander@myxt.nl>
2011-07-14 12:30:33 +0200

    Update.

--- settings/override/site.ini.append.php
+++ settings/override/site.ini.append.php
@@ -30,31 +30,14 @@ SiteList[]=myxt_user
 SiteList[]=myxt_admin
 SiteList[]=myxt_mobile
 
-SiteList[]=f2m_user_eu
-SiteList[]=f2m_user_nl
-SiteList[]=f2m_user_de
-SiteList[]=f2m_user_be
-SiteList[]=f2m_admin
-
 [SiteAccessSettings]
 AvailableSiteAccessList[]=myxt_user
 AvailableSiteAccessList[]=myxt_admin
 AvailableSiteAccessList[]=myxt_mobile
 
-AvailableSiteAccessList[]=f2m_user_eu
-AvailableSiteAccessList[]=f2m_user_nl
-AvailableSiteAccessList[]=f2m_user_de
-AvailableSiteAccessList[]=f2m_admin
-
 MatchOrder=uri;host
-HostMatchMapItems[]=admin.ez.localhost;myxt_admin
-HostMatchMapItems[]=www.ez.localhost;myxt_user
-HostMatchMapItems[]=mobile.ez.localhost;myxt_mobile
-
-HostMatchMapItems[]=www.food2market.localhost;f2m_user_eu
-HostMatchMapItems[]=www.food2market.nl.localhost;f2m_user_nl
-HostMatchMapItems[]=www.food2market.de.localhost;f2m_user_de
-HostMatchMapItems[]=www.food2market.be.localhost;f2m_user_be
-HostMatchMapItems[]=webadmin.food2market.localhost;f2m_admin
+HostMatchMapItems[]=webadmin.ez.localhost;site_admin
+HostMatchMapItems[]=www.ez.localhost;site_user
+HostMatchMapItems[]=m.ez.localhost;site_mobile
 
 */ ?>
