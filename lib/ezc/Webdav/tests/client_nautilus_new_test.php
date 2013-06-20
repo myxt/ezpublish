10c4147772e20e122885daade894d9ac2cc306c8
Sander van den Akker <sander@myxt.nl>
2013-06-20 12:17:36 +0200

    Integrated ezc.

---
+++ lib/ezc/Webdav/tests/client_nautilus_new_test.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Client test for Nautilus (new).
+ *
+ * @package Webdav
+ * @subpackage Tests
+ * @version 1.1.3
+ * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/new_bsd New BSD License
+ */
+
+require_once 'client_test_suite.php';
+require_once 'client_test_continuous_setup.php';
+
+/**
+ * Client test for Nautilus (new).
+ * 
+ * @package Webdav
+ * @subpackage Tests
+ */
+class ezcWebdavNautilusNewClientTest extends ezcTestCase
+{
+    public static function suite()
+    {
+        return new ezcWebdavClientTestSuite(
+            'Nautilus (new)',
+            'clients/nautilus_new.php',
+            new ezcWebdavClientTestContinuousSetup()
+        );
+    }
+}
+
+?>
